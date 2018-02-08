32e933ecdff8615384d6d3be8c2f8df24d4d1470
duxin <hardboy_du@163.com>
2018-02-08 15:14:45 +0800

    Add files via upload

---
+++ notebook.mm
@@ -0,0 +1,136 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="notebook" LOCALIZED_STYLE_REF="styles.topic" FOLDED="false" ID="ID_1408217209" CREATED="1518053096141" MODIFIED="1518059115943"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="21" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Linux" POSITION="right" ID="ID_1876716592" CREATED="1518054470595" MODIFIED="1518068022697">
+<icon BUILTIN="penguin"/>
+<edge COLOR="#007c7c"/>
+<node TEXT="Kernel" ID="ID_220744235" CREATED="1518054696851" MODIFIED="1518054835953">
+<node TEXT="mm" ID="ID_1679806373" CREATED="1518055043992" MODIFIED="1518055047160"/>
+</node>
+<node TEXT="Administration" ID="ID_676056537" CREATED="1518054702939" MODIFIED="1518054711332"/>
+<node TEXT="Performance" ID="ID_1885586474" CREATED="1518058498060" MODIFIED="1518058502642"/>
+</node>
+<node TEXT="Architecture" POSITION="right" ID="ID_1777227061" CREATED="1518054566634" MODIFIED="1518068063929">
+<edge COLOR="#0000ff"/>
+<node TEXT="TextBook" ID="ID_1852971469" CREATED="1518055100184" MODIFIED="1518057847824">
+<node TEXT="Computer Systems: A Programmer&apos;s Perspective" ID="ID_1798379254" CREATED="1518058412115" MODIFIED="1518058429548"/>
+<node TEXT="Computer Architecture: A Quantitative Approach" ID="ID_191025742" CREATED="1518058436330" MODIFIED="1518058677925"/>
+</node>
+</node>
+<node TEXT="Algorithms" POSITION="right" ID="ID_119681019" CREATED="1518054498107" MODIFIED="1518054499468">
+<edge COLOR="#7c7c00"/>
+<node TEXT="TextBook" ID="ID_1637181969" CREATED="1518058875983" MODIFIED="1518058880162"/>
+<node TEXT="Focus" ID="ID_367080889" CREATED="1518059223790" MODIFIED="1518059257649"/>
+<node TEXT="Puzzles" ID="ID_1452021914" CREATED="1518059292127" MODIFIED="1518059300896"/>
+</node>
+<node TEXT="Compiler" POSITION="right" ID="ID_405484878" CREATED="1518055020160" MODIFIED="1518055024530">
+<edge COLOR="#ff00ff"/>
+<node TEXT="TextBook" ID="ID_1731845765" CREATED="1518059423427" MODIFIED="1518059429349"/>
+<node TEXT="Focus" ID="ID_163880490" CREATED="1518059431131" MODIFIED="1518059435775"/>
+</node>
+<node TEXT="Computer Network" POSITION="right" ID="ID_1056651116" CREATED="1518059329347" MODIFIED="1518059337544">
+<edge COLOR="#00ffff"/>
+<node TEXT="Focus" ID="ID_1280847106" CREATED="1518059351851" MODIFIED="1518059356271">
+<node TEXT="Packet Process" ID="ID_1386660555" CREATED="1518059358288" MODIFIED="1518059365546">
+<node TEXT="DPDK" ID="ID_955156109" CREATED="1518059373043" MODIFIED="1518059381031"/>
+<node TEXT="PF_ring" ID="ID_483558467" CREATED="1518059383989" MODIFIED="1518059388003"/>
+</node>
+<node TEXT="Network Programming" ID="ID_1068194645" CREATED="1518072853563" MODIFIED="1518072866809">
+<node TEXT="epoll" ID="ID_1788093966" CREATED="1518072866813" MODIFIED="1518072890035"/>
+<node TEXT="nginx" ID="ID_1817900528" CREATED="1518072891690" MODIFIED="1518073555348">
+<node TEXT="nginx" ID="ID_1504318569" CREATED="1518073555352" MODIFIED="1518073559017"/>
+<node TEXT="openresty" ID="ID_1321792560" CREATED="1518073560853" MODIFIED="1518073564563"/>
+<node TEXT="tengine" ID="ID_455824011" CREATED="1518073616341" MODIFIED="1518073618511"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Parallel and Concurrency" POSITION="right" ID="ID_1362626906" CREATED="1518069634054" MODIFIED="1518069636431">
+<edge COLOR="#7c0000"/>
+<node TEXT="Courses" ID="ID_9179533" CREATED="1518069767222" MODIFIED="1518069769294">
+<node TEXT="Parallel Computer Architecture and Programming (CMU 15-418/618)" ID="ID_139517476" CREATED="1518069797644" MODIFIED="1518069871140" LINK="http://15418.courses.cs.cmu.edu"/>
+</node>
+<node TEXT="Focurs" ID="ID_398562216" CREATED="1518069907748" MODIFIED="1518069914332">
+<node TEXT="Lock-Free" ID="ID_1099113362" CREATED="1518069914337" MODIFIED="1518069920067"/>
+</node>
+</node>
+<node TEXT="Computer Security" POSITION="right" ID="ID_1862946564" CREATED="1518070165947" MODIFIED="1518070172579">
+<edge COLOR="#00007c"/>
+<node TEXT="Cryptography" ID="ID_1800975030" CREATED="1518070712975" MODIFIED="1518070714677"/>
+</node>
+<node TEXT="Web Development" POSITION="right" ID="ID_426395321" CREATED="1518070184321" MODIFIED="1518070191540">
+<edge COLOR="#007c00"/>
+<node TEXT="Front-End" ID="ID_1797961235" CREATED="1518072416099" MODIFIED="1518072423788"/>
+</node>
+<node TEXT="Artificial Intelligence" POSITION="right" ID="ID_1112364198" CREATED="1518070314273" MODIFIED="1518070316247">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="Programming Language" POSITION="right" ID="ID_1233371444" CREATED="1518070477153" MODIFIED="1518070483208">
+<edge COLOR="#007c7c"/>
+<node TEXT="C" ID="ID_785974178" CREATED="1518072147598" MODIFIED="1518072152094"/>
+<node TEXT="C++" ID="ID_1199086481" CREATED="1518072153495" MODIFIED="1518072157858"/>
+<node TEXT="Python" ID="ID_704633055" CREATED="1518072159791" MODIFIED="1518072163374"/>
+</node>
+</node>
+</map>
